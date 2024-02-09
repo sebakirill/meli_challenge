ddca2c37d2ab2187c0e43a0c797caf7a3cd0e341
Sebastian Cabrera <sebastiankirill.cabrera@mercadolibre.com>
2024-02-09 17:10:55 -0300

    Fix order of steps in preprocess pipeline
    Changes to be committed:
    	modified:   src/preprocess/feature_selection.py

--- src/preprocess/feature_selection.py
+++ src/preprocess/feature_selection.py
@@ -6,7 +6,7 @@ from src.preprocess.imputing import simple_imputer
 from src.preprocess.encoding import freq_encoder
 
 
-def pipe_feature_selection():
+def pipe_feature_selection() -> Pipeline:
     """
     Create a pipeline for feature selection using XGBoost.
 
@@ -24,8 +24,8 @@ def pipe_feature_selection():
     pipe_feature_selection = Pipeline(
         [
             ("reduce_memory", ReduceMemoryUsageTransformer()),
-            ("freq_encoder", freq_encoder()),
             ("simple_imputer", simple_imputer()),
+            ("freq_encoder", freq_encoder()),
             (
                 "xgb_class",
                 XGBClassifier(objective="binary:logistic", enable_categorical=True),

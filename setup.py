142a1aafbca5dfc1b40f1db666662b3b924217d7
Sebastian Cabrera <sebastiankirill.cabrera@mercadolibre.com>
2024-02-09 11:34:30 -0300

    Changes to be committed: 	new file:   setup.py 	new file:   src/features/__init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meli_challenge",
+    version="0.1",
+    packages=find_packages(),
+    author="Sebastian Kirill Cabrera",
+    author_email="sebastiankirill.cabrera@mercadolibre.com",
+    description="",
+    url="",
+)

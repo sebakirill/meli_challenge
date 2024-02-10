f717efa61168ea8f6dd8e2ea87628637eaa4f70a
Sebastian Cabrera <sebastiankirill.cabrera@mercadolibre.com>
2024-02-10 19:16:46 -0300

    Add XGBoost, LightGBM, Random Forest and Logistic Regression
    Changes to be committed:
    	modified:   src/model/models.py

--- src/model/models.py
+++ src/model/models.py
@@ -4,31 +4,98 @@ from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 
 
-def log_reg(C, fit_intercept, solver, trial=None):
+def log_reg(C, fit_intercept, solver, random_state, trial=None):
     return LogisticRegression(
         C=trial.suggest_loguniform("C", **C),
         fit_intercept=trial.suggest_categorical("fit_intercept", fit_intercept),
         solver=trial.suggest_categorical("solver", solver),
-        n_jobs=-1,
-        random_state=123,
+        random_state=trial.suggest_int("random_state", **random_state),
     )
 
 
-def random_forest(n_estimators, max_depth, min_samples_split, trial=None):
+def random_forest(
+    n_estimators,
+    max_depth,
+    min_samples_split,
+    bootstrap,
+    random_state,
+    n_jobs,
+    trial=None,
+):
     return RandomForestClassifier(
         n_estimators=trial.suggest_int("n_estimators", **n_estimators),
         max_depth=trial.suggest_int("max_depth", **max_depth),
         min_samples_split=trial.suggest_discrete_uniform(
             "min_samples_split", **min_samples_split
         ),
-        n_jobs=-1,
-        random_state=123,
+        n_jobs=trial.suggest_int("n_jobs", **n_jobs),
+        random_state=trial.suggest_int("random_state", **random_state),
+        bootstrap=trial.suggest_categorical("bootstrap", **bootstrap),
     )
 
 
-def xgboost_mod(trial=None):
-    return XGBClassifier()
+def xgboost_mod(
+    learning_rate,
+    min_child_weight,
+    subsample,
+    colsample_bytree,
+    objective,
+    scale_pos_weight,
+    n_jobs,
+    random_state,
+    n_estimators,
+    max_depth,
+    min_samples_split,
+    trial=None,
+):
+
+    return XGBClassifier(
+        learning_rate=trial.suggest_discrete_uniform("learning_rate", **learning_rate),
+        min_child_weight=trial.suggest_int("min_child_weight", **min_child_weight),
+        subsample=trial.suggest_discrete_uniform("subsample", **subsample),
+        colsample_bytree=trial.suggest_discrete_uniform(
+            "colsample_bytree", **colsample_bytree
+        ),
+        scale_pos_weight=trial.suggest_int("scale_pos_weight", scale_pos_weight),
+        n_jobs=trial.suggest_int("n_jobs", n_jobs),
+        random_state=trial.suggest_int("random_state", random_state),
+        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        max_depth=trial.suggest_int("max_depth", **max_depth),
+        min_samples_split=trial.suggest_discrete_uniform(
+            "min_samples_split", **min_samples_split
+        ),
+        objective=trial.suggest_categorical("objective", objective),
+    )
 
 
-def lightgmb_mod(trial=None):
-    return LGBMClassifier()
+def lightgmb_mod(
+    learning_rate,
+    min_child_weight,
+    subsample,
+    colsample_bytree,
+    objective,
+    is_unbalance,
+    n_jobs,
+    random_state,
+    n_estimators,
+    max_depth,
+    min_samples_split,
+    trial=None,
+):
+    return LGBMClassifier(
+        learning_rate=trial.suggest_discrete_uniform("learning_rate", **learning_rate),
+        min_child_weight=trial.suggest_int("min_child_weight", **min_child_weight),
+        subsample=trial.suggest_discrete_uniform("subsample", **subsample),
+        colsample_bytree=trial.suggest_discrete_uniform(
+            "colsample_bytree", **colsample_bytree
+        ),
+        objective=trial.suggest_categorical("objective", objective),
+        is_unbalance=trial.suggest_categorical("is_unbalance", is_unbalance),
+        n_jobs=trial.suggest_int("n_jobs", n_jobs),
+        random_state=trial.suggest_int("random_state", random_state),
+        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        max_depth=trial.suggest_int("max_depth", **max_depth),
+        min_samples_split=trial.suggest_discrete_uniform(
+            "min_samples_split", **min_samples_split
+        ),
+    )

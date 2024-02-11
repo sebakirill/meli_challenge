7f3b461d10a5ca9de54cf5dbbeec45b4d36faaa4
Sebastian Cabrera <sebastiankirill.cabrera@gmail.com>
2024-02-11 19:41:14 -0300

    Changes to be committed: 	modified:   conf/config.yaml 	new file:   conf/data/class_weight/class_weight.yaml 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import optuna
 from optuna.samplers import TPESampler
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
+from src.utils.utils import class_weight
 from src.data.make_dataset import get_Xs_ys, ReduceMemoryUsageTransformer
 from src.preprocess.imputing import simple_imputer, drop_na
 from src.preprocess.encoding import FrequencyEncoder
@@ -17,8 +18,8 @@ from src.preprocess.feature_selection import (
 
 @hydra.main(config_path="../conf", config_name="config", version_base="1.3")
 def train_model(cfg: DictConfig):
-    # hydra.utils.call(cfg.data.extract_zip.type)
     X_train, X_test, y_train, y_test = hydra.utils.call(cfg.data.get_Xs_ys.type)
+    hydra.utils.call(cfg.data.class_weight.type, df=y_train)
     # pipe_feature_selection = hydra.utils.call(cfg.feature_selection.type)
     # pipe_feature_selection.fit(X_train,y_train)
     # hydra.utils.call(cfg.save_selected_columns.type, pipeline=pipe_feature_selection)

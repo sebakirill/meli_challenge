05ffa5a483186a1e8ec22221e504235ff9888866
Sebastian Cabrera <sebastiankirill.cabrera@mercadolibre.com>
2024-02-11 09:23:33 -0300

    Create pipe_preprocess pipe_preprocess
    Changes to be committed:
    	modified:   conf/preprocess/encoding/freq_encoder.yaml
    	modified:   conf/preprocess/encoding/one_hot_encoder.yaml
    	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import hydra
 from omegaconf import DictConfig
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 from src.data.make_dataset import get_Xs_ys, ReduceMemoryUsageTransformer
 from src.preprocess.imputing import simple_imputer, drop_na
-from src.preprocess.encoding import one_hot_encoder, freq_encoder
+from src.preprocess.encoding import FrequencyEncoder
 from src.preprocess.feature_selection import (
     pipe_feature_selection,
     save_selected_columns,
 )
-from sklearn.pipeline import Pipeline
+
 
 
 @hydra.main(config_path="../conf", config_name="config", version_base="1.3")
@@ -17,6 +19,19 @@ def train_model(cfg: DictConfig):
     X_train, X_test, y_train, y_test = hydra.utils.call(cfg.data.get_Xs_ys.type)
     pipe_feature_selection = hydra.utils.call(cfg.feature_selection.type)
     pipe_feature_selection.fit(X_train,y_train)
+    
+    pipe_preprocess = Pipeline([
+        ("reduce_memory", ReduceMemoryUsageTransformer()),
+        ("imputer", hydra.utils.call(cfg.prerocess.encoding.type)),
+        ("encoding", hydra.utils.instantiate(cfg.preprocess.imputing.type))
+    ])
 
+    pipe_model = Pipeline([
+        ("pipe_prep", pipe_preprocess),
+        ("model", hydra.utils.call(cfg.models.type))
+    ])
+
+    pipe_model.fit(X_train, y_train)
+    
 if __name__ == "__main__":
     train_model()

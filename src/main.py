1bace092a88059cc5b10ccc9ef803fe3bdb4ed65
Sebastian Cabrera <sebastiankirill.cabrera@gmail.com>
2024-02-12 02:09:48 -0300

    Test random forest ok
    Changes to be committed:
    	modified:   best_params.yaml
    	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import OneHotEncoder
 from src.utils.utils import class_weight
 from src.data.make_dataset import get_Xs_ys, ReduceMemoryUsageTransformer
 from src.preprocess.encoding import FrequencyEncoder
-from src.preprocess.imputing import drop_na, cat_imputer, num_imputer
+from src.preprocess.imputing import cat_imputer, num_imputer
 from src.preprocess.feature_selection import (
     pipe_feature_selection,
     save_selected_columns,
@@ -21,18 +21,15 @@ from src.model.models import log_reg, random_forest, xgboost_mod, lightgmb_mod
 
 @hydra.main(config_path="../conf", config_name="config", version_base="1.3")
 def train_model(cfg: DictConfig):
+    
     X_train, X_test, y_train, y_test = hydra.utils.call(cfg.data.get_Xs_ys.type)
     hydra.utils.call(cfg.data.class_weight.type, df=y_train)
     pipe_feature_selection = hydra.utils.call(cfg.feature_selection.type)
-    pipe_feature_selection.fit(X_train,y_train)
-    hydra.utils.call(cfg.save_selected_columns.type, pipeline=pipe_feature_selection)
+    hydra.utils.call(cfg.save_selected_columns.type, pipeline=pipe_feature_selection.fit(X_train,y_train))
     
     def optimize_model(trial):
         
-        memory_pipe = Pipeline([
-            ("reduce_memory", hydra.utils.instantiate(cfg.data.reduce_memory_usage.type)),
-        ])
-        
+        memory_pipe = hydra.utils.call(cfg.data.reduce_memory_usage.type)
         preprocess_pipe= ColumnTransformer(
             transformers=[
                 ('cat', Pipeline([
@@ -43,7 +40,6 @@ def train_model(cfg: DictConfig):
                     ("imputing", hydra.utils.call(cfg.preprocess.imputing.num_imputer.type, trial=trial)), 
                 ]), memory_pipe.fit_transform(X_train).select_dtypes('number').columns),
             ])
-        
         model_pipe = Pipeline([
             ("pipe_prep", memory_pipe),
             ("pipe_end", preprocess_pipe),

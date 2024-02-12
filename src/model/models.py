7cdbdecb869a86c33a6ab120da941421ea07b3a0
Sebastian Cabrera <sebastiankirill.cabrera@gmail.com>
2024-02-12 01:03:46 -0300

    Fix models params
    Changes to be committed:
    	modified:   conf/models/lgmb_clas.yaml
    	modified:   conf/models/log_reg.yaml
    	modified:   src/model/models.py

--- src/model/models.py
+++ src/model/models.py
@@ -4,36 +4,38 @@ from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 
 
-def log_reg(C, fit_intercept, solver, random_state, class_weight, trial=None):
+def log_reg(C, class_weight, fit_intercept, solver, random_state, trial=None):
     return LogisticRegression(
         C=trial.suggest_loguniform("C", **C),
+        class_weight=trial.suggest_int("class_weight", class_weight),
         fit_intercept=trial.suggest_categorical("fit_intercept", fit_intercept),
         solver=trial.suggest_categorical("solver", solver),
-        random_state=trial.suggest_int("random_state", **random_state),
-        class_weight=trial.suggest_int("class_weight", class_weight)
+        random_state=random_state
     )
 
 
 def random_forest(
-    n_estimators,
-    max_depth,
-    min_samples_split,
     bootstrap,
-    random_state,
-    n_jobs,
     class_weight,
+    max_depth,
+    min_samples_leaf,
+    min_samples_split,
+    n_estimators,
+    n_jobs,
+    random_state,
     trial=None,
 ):
     return RandomForestClassifier(
-        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        bootstrap=trial.suggest_categorical("bootstrap", **bootstrap),
+        class_weight=class_weight,
         max_depth=trial.suggest_int("max_depth", **max_depth),
+        min_samples_leaf=trial.suggest_init("min_samples_split", **min_samples_leaf),
         min_samples_split=trial.suggest_discrete_uniform(
             "min_samples_split", **min_samples_split
         ),
-        n_jobs=trial.suggest_int("n_jobs", **n_jobs),
-        random_state=trial.suggest_int("random_state", **random_state),
-        bootstrap=trial.suggest_categorical("bootstrap", **bootstrap),
-        class_weight=trial.suggest_int("class_weight", class_weight)
+        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        n_jobs=n_jobs,
+        random_state=random_state,
     )
 
 
@@ -72,33 +74,29 @@ def xgboost_mod(
 
 
 def lightgmb_mod(
-    learning_rate,
-    min_child_weight,
-    subsample,
     colsample_bytree,
-    objective,
     is_unbalance,
-    n_jobs,
-    random_state,
-    n_estimators,
+    learning_rate,
     max_depth,
-    min_samples_split,
+    min_child_weight,
+    n_estimators,
+    n_jobs,
+    objective,
+    random_state,
+    subsample,
     trial=None,
 ):
     return LGBMClassifier(
-        learning_rate=trial.suggest_discrete_uniform("learning_rate", **learning_rate),
-        min_child_weight=trial.suggest_int("min_child_weight", **min_child_weight),
-        subsample=trial.suggest_discrete_uniform("subsample", **subsample),
         colsample_bytree=trial.suggest_discrete_uniform(
             "colsample_bytree", **colsample_bytree
         ),
-        objective=trial.suggest_categorical("objective", objective),
-        is_unbalance=trial.suggest_categorical("is_unbalance", is_unbalance),
-        n_jobs=trial.suggest_int("n_jobs", n_jobs),
-        random_state=trial.suggest_int("random_state", random_state),
-        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        is_unbalance=is_unbalance,
+        learning_rate=trial.suggest_discrete_uniform("learning_rate", **learning_rate),
         max_depth=trial.suggest_int("max_depth", **max_depth),
-        min_samples_split=trial.suggest_discrete_uniform(
-            "min_samples_split", **min_samples_split
-        ),
+        min_child_weight=trial.suggest_int("min_child_weight", **min_child_weight),
+        n_estimators=trial.suggest_int("n_estimators", **n_estimators),
+        n_jobs=n_jobs,
+        objective=objective,
+        random_state=random_state,
+        subsample=trial.suggest_discrete_uniform("subsample", **subsample),
     )
